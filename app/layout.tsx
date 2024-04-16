c7c63c38fa9d9185c7ef42af9b949215ce77db67
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-04-16 18:45:31 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <ChakraProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ChakraProvider>
+      </body>
     </html>
   );
 }
